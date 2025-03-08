40ee9ed5e33682033602618f00a906bb0074136c
Mohamad Nur Tawakal <mohamadnurtawakal@MacBook-Air-Mohamad.local>
2025-03-08 17:27:58 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/web-desa/",
+  base: "/anythings10.github.io/",
   plugins: [react()],
   build: {
     outDir: "dist",
